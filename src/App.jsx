4ad6a277957108b4e365bfc18bccf2bc43622cbc
Shp981 <shuertas981@gmail.com>
2024-02-14 09:42:04 -0500

    agregando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import './index.css'
 import { CrearProfesor } from './components/CrearProfesor';
 function App() {
   return (
     <>
-    <button className='p-2 bg-purple-600 text-white' >Crear Profesor</button>
+    <button className='p-2 bg-purple-600 text-white py-3' >Crear Profesor</button>
     <CrearProfesor  />
   </>
   )
